8a92edba765aff97d30de43ae7d4182385faa443
Ruslan Rozhkov <bladteam@gmail.com>
2017-01-20 20:09:54 +0200

    tire

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ApplicationRecord
+  include Tire::Model::Search
+  include Tire::Model::Callbacks
+
+  ac_field :username
+
+  users = [
+             {username: 'Mike', image_url: 'http://pickaface.net/includes/themes/clean/img/slide2.png'},
+             {username: 'James', image_url: 'http://pickaface.net/includes/themes/clean/img/slide4.png'},
+             {username: 'Kane', image_url: 'http://pickaface.net/includes/themes/clean/img/slide5.png'},
+             {username: 'Jessica', image_url: 'http://pickaface.net/includes/themes/clean/img/slide1.png'},
+             {username: 'Tiago', image_url: 'http://findicons.com/files/icons/1072/face_avatars/300/g04.png'}
+         ]
+
+  Tire.index 'user_index' do
+    import users
+    end
 
 end
